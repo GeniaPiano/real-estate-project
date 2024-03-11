c9136053942720867cbda42a3d19239c9f4f9b11
GeniaPiano <genia.piano@hotmail.com>
2024-03-11 21:26:26 +0100

    Add sass and theme with customo styles

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
 dotenv.config()
-import viteSass from 'vite-plugin-sass'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),  viteSass()]
+  plugins: [react()]
 })
