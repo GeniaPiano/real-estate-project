535a400ecf747bd3e7d1165710b2e87a20a67fff
GeniaPiano <genia.piano@hotmail.com>
2024-03-11 21:26:41 +0100

    Add theme with custom styles

---
+++ src/assets/theme.js
@@ -0,0 +1,21 @@
+import { createTheme } from '@mui/material/styles'
+
+export const theme = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#0aa4a8',
+      green: '#27d07d',
+      black: '#000000',
+      white: '#ffffff',
+      blue: '#0992ee96',
+    },
+    secondary: {
+      main: '#f50057',
+      grey: '#B5B5B5',
+      greyLight: '#EBEBEB',
+      greyDark: '#767373',
+      anthracite: '#1A1A1A'
+    }
+  }
+})
