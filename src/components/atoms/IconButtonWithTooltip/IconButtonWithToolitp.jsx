c1b453d94daacd08f933fc964a997c0a5f24dec2
GeniaPiano <genia.piano@hotmail.com>
2024-03-12 10:42:33 +0100

    Create IconButton with toolip and Tittle Page components

---
+++ src/components/atoms/IconButtonWithTooltip/IconButtonWithToolitp.jsx
@@ -0,0 +1,27 @@
+import { IconButton, Tooltip } from '@mui/material'
+import { PropTypes } from 'prop-types'
+
+export const IconButtonWithTooltip = ({icon, title, ariaLabel, handleClick, color, disabled = false}) => {
+
+return (
+    <Tooltip title={title}>
+      <IconButton 
+          aria-label={ariaLabel}  
+          onClick={handleClick}  
+          color={color || "primary"} 
+          variant="contained" 
+          disabled={disabled}>
+        {icon}
+      </IconButton>
+    </Tooltip>
+  )  
+}
+
+IconButtonWithTooltip.propTypes = {
+  icon: PropTypes.element.isRequired,
+  title: PropTypes.string.isRequired,
+  ariaLabel: PropTypes.string,
+  handleClick: PropTypes.func,
+  color: PropTypes.string,
+  disabled: PropTypes.bool
+}
--- src/components/atoms/TitlePage/TitlePage.jsx
+++ src/components/atoms/TitlePage/TitlePage.jsx
@@ -1,11 +1,17 @@
 import { Typography, Box } from '@mui/material'
+import { PropTypes } from 'prop-types'
 
-export const TitlePage = ({title}) => {
+export const TitlePage = ({title, sx}) => {
   return (
-    <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '30px'}}>
-      <Typography sx={{fontSize: '22px', fontWeight: 'bold', textAlign: 'center'}}>
+    <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '30px',  sx}}>
+      <Typography sx={{fontSize: '22px', fontWeight: 'bold', textAlign: 'center', ...sx}}>
         {title}
       </Typography>
     </Box>
   )
+}
+
+TitlePage.prototype = {
+  title: PropTypes.string.isRequired,
+  sx: PropTypes.object
 }
