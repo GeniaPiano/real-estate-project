1ba50be364c6346d201e24db5187fd215a67196a
GeniaPiano <genia.piano@hotmail.com>
2024-03-14 23:27:28 +0100

    Create NotFoundProperties

---
+++ src/components/atoms/NotFoundProperties/NotFoundProperties.jsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from "@mui/material"
+import { useTheme,  alpha } from "@mui/material/styles"
+
+export const NotFoundProperties = ({message}) => {
+  const theme = useTheme()
+  return (
+    <Box  sx={{background: alpha(theme.palette.secondary.main, 0.2), borderRadius: '10px', paddingY: '50px', marginTop: '20px'}}>
+      <Typography variant="h5" sx={{textAlign: 'center', color: theme.palette.secondary.greyDark}}>
+        {message}
+      </Typography> 
+     </Box>
+  )
+}
