9c00c3e411b6bb8dd0242efc3beb061531194c3c
GeniaPiano <genia.piano@hotmail.com>
2024-03-14 16:40:33 +0100

    Remove unused stuff

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,5 @@
 import { PropertiesList } from '../../components/organisms/PropertiesList/ProperstiesList'
 
-const apiUrl = import.meta.env.VITE_REACT_APP_API_URL
-
 export const HomePage = () =>    
     <PropertiesList />
  
