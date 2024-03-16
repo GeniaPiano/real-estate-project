fb2b0047c03a501c82efe14639334be37c988ffd
GeniaPiano <genia.piano@hotmail.com>
2024-03-16 10:15:40 +0100

    Add propertiesListTest

--- src/components/organisms/PropertiesList/PropertiesList.test.jsx
+++ src/components/organisms/PropertiesList/PropertiesList.test.jsx
@@ -3,13 +3,10 @@ import { render, screen, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom';
 import { PropertiesList } from './ProperstiesList.jsx'
 import { usePropertiesStore } from '../../../stores/usePropertiesStore'
-
-// Mock the usePropertiesStore hook
 jest.mock('../../../stores/usePropertiesStore', () => ({
   usePropertiesStore: jest.fn(),
-}));
+}))
 
-// Mock the useNavigate hook
 jest.mock('react-router', () => ({
   useNavigate: jest.fn(),
 }));
@@ -29,16 +26,15 @@ describe('PropertiesList', () => {
 
   it('renders properties when data is fetched', async () => {
     const mockProperties = [
-      { id: 1, name: 'Property 1', city: 'City 1', price: '1000', images: ['url1'], description: 'Description 1' },
-  
-    ];
+      { id: 1, name: 'Property 1', city: 'City 1', price: '1000', images: ['url1'], description: 'Description 1' }  
+    ]
 
     usePropertiesStore.mockImplementation(() => ({
       properties: mockProperties,
       fetchProperties: jest.fn(),
       isLoading: false,
       filteredProperties: mockProperties,
-    }));
+    }))
 
     render(<PropertiesList />);
 
