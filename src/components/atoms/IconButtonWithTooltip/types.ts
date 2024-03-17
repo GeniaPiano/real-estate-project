75986823fcc71943e8b37e6b7f137b5e9fc71436
GeniaPiano <genia.piano@hotmail.com>
2024-03-17 02:20:10 +0100

    Add types to IconButtonWithToolitp.tsx

---
+++ src/components/atoms/IconButtonWithTooltip/types.ts
@@ -0,0 +1,12 @@
+import React from "react";
+import {TooltipProps} from "@mui/material/Tooltip";
+import {IconButtonProps} from "@mui/material/IconButton";
+
+export interface IconButtonWithTooltipProps {
+    icon: React.ReactNode;
+    title: TooltipProps['title'];
+    ariaLabel: string;
+    handleClick: () => void;
+    color?: IconButtonProps['color'];
+    disabled?: boolean;
+}
