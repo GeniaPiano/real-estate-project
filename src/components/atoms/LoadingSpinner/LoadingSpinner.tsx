afafdc3960c9d4d1a26647c426f2f9ce79533c0f
GeniaPiano <genia.piano@hotmail.com>
2024-03-17 02:18:39 +0100

    Add messages to LoadingSpinner.tsx

--- src/components/atoms/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/atoms/LoadingSpinner/LoadingSpinner.tsx
@@ -1,9 +1,10 @@
 import { Box, CircularProgress, Typography } from '@mui/material';
 import {style} from "./style.ts";
+import {messages} from "./messages.ts";
 
 export const LoadingSpinner = () => (
   <Box sx={style.wrapper}>
         <CircularProgress />
-        <Typography>Loading... </Typography>    
+        <Typography> {messages.loading} </Typography>
   </Box>
 )
