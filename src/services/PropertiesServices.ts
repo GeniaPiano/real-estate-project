0759735cd8e3f2173567a87f7d6006f8c51ea7c4
GeniaPiano <genia.piano@hotmail.com>
2024-03-17 11:18:47 +0100

    Add types to PropertiesServices.ts

--- src/services/PropertiesServices.ts
+++ src/services/PropertiesServices.ts
@@ -1,13 +1,25 @@
-import axios from 'axios'
+import axios from 'axios';
+import {Property} from "../types/Property.ts";
 
 export const propertiesService = {
-  fetchAll: async () => {
-    const propertiesFromApi = await axios.get('./db.json')
-    return propertiesFromApi.data.properties
-  },
-  fetchById: async (id) => {
-    const allProperties = await propertiesService.fetchAll()
-    if (allProperties.length > 0)
-    return allProperties.find(property => Number(property.id) === Number(id))
-  }
-}
+    fetchAll: async () => {
+        try {
+            const response = await axios.get('/db.json');
+            return response.data.properties as Property[];
+        } catch (error) {
+            console.error("Error fetching properties: ", error);
+            return [];
+        }
+    },
+
+    fetchById: async (id: string) => {
+        try {
+            const allProperties : Property[] = await propertiesService.fetchAll();
+            const property : Property | undefined = allProperties.find(property => Number(property.id) === Number(id));
+            return property || null;
+        } catch (error) {
+            console.error("Error fetching property by ID: ", error);
+            return null;
+        }
+    }
+};
