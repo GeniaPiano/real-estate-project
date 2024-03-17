627d027c0b46084a973a0165ccc0cfc584b0b95c
GeniaPiano <genia.piano@hotmail.com>
2024-03-17 10:23:23 +0100

    Create Country and Property interface

---
+++ src/types/Country.ts
@@ -0,0 +1,5 @@
+export interface Country {
+    countryName: string;
+    map: string;
+    flag: string;
+}
---
+++ src/types/Property.ts
@@ -0,0 +1,10 @@
+export interface Property {
+    id: number;
+    name: string;
+    type: string;
+    description: string;
+    price: number;
+    surface: number;
+    images: string[];
+    city: string;
+}
